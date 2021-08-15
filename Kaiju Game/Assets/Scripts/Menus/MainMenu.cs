b7ff228be3f5f078cf99fb9b12d0bcd2912e0d7d
Brodie Hill <brodiehill79@gmail.com>
2021-08-16 06:19:54 +1200

    Add empty prototyping scene, linked to main menu

--- Kaiju Game/Assets/Scripts/Menus/MainMenu.cs
+++ Kaiju Game/Assets/Scripts/Menus/MainMenu.cs
@@ -1,21 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] DialogBox quitConfirmationBox = null;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     public void TryQuit()
     {
@@ -24,5 +15,10 @@ public class MainMenu : MonoBehaviour
     public void ConfirmQuit()
     {
         Application.Quit();
+    }
+
+    public void LoadPrototypeScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
