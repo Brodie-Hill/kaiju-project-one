d9205732305ba0ed1162df10ca906dd08848c383
Pedro Munoz <pedromunoz.dev@gmail.com>
2021-09-12 10:27:44 -0500

    updated animation

--- Kaiju Game/Assets/Scripts/TwoDimensionalAnimationScriptController.cs
+++ Kaiju Game/Assets/Scripts/TwoDimensionalAnimationScriptController.cs
@@ -9,6 +9,8 @@ public class TwoDimensionalAnimationScriptController : MonoBehaviour
     float velocityX = 0.0f;
     public float acceleration = 2.0f;
     public float deceleration = 2.0f;
+    public float maximumWalkVelocity = 0.05f;
+    public float maximumRunVelocity = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,21 +29,24 @@ public class TwoDimensionalAnimationScriptController : MonoBehaviour
         bool rightPressed = Input.GetKey("d");
         bool runPressed = Input.GetKey("left shift");
 
+        // set current maxVelocity
+        float currentMaxVelocity = runPressed ? maximumRunVelocity : maximumWalkVelocity;
+
 
         // if player presses forward, increase velocity in z direction
-        if (forwardPressed && velocityZ < 0.5f && !runPressed)
+        if (forwardPressed && velocityZ < currentMaxVelocity)
         {
             velocityZ += Time.deltaTime * acceleration;
         }
 
         // increase velocity in left direction
-        if (leftPressed && velocityX > -0.5f)
+        if (leftPressed && velocityX > - currentMaxVelocity)
         {
             velocityX -= Time.deltaTime * acceleration;
         }
 
         // increase velocity in right direction
-        if (rightPressed && velocityX <0.5f && !runPressed)
+        if (rightPressed && velocityX < currentMaxVelocity)
         {
             velocityX += Time.deltaTime * acceleration;
         }
@@ -74,6 +79,69 @@ public class TwoDimensionalAnimationScriptController : MonoBehaviour
         if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
         {
             velocityX = 0.0f;
+        }
+
+        // lock forward
+        if (forwardPressed && runPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ = currentMaxVelocity;
+        }
+        // decelerate to maximum walk velocity
+        else if (forwardPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+            // round to the curentMaxVelocity if within offset
+            if (velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05f))
+            {
+                velocityZ = currentMaxVelocity;
+            }
+        }
+        // round to the currentMaxVelocity if within offset
+        else if (forwardPressed && velocityZ < currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.05f))
+        {
+            velocityZ = currentMaxVelocity;
+        }
+        
+        // lock left
+        if (leftPressed && runPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX = -currentMaxVelocity;
+        }
+        // decelerate to maximum walk velocity 
+        else if (leftPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * deceleration;
+            // round to the curentMaxVelocity if within offset
+            if (velocityX < -currentMaxVelocity && velocityX > (-currentMaxVelocity - 0.05f)) {
+                velocityX = -currentMaxVelocity;
+            }
+        }
+        // round to the currentMaxVelocity if within offset
+        else if (leftPressed && velocityX >  -currentMaxVelocity && velocityX < (-currentMaxVelocity + 0.05f))
+        {
+            velocityX = -currentMaxVelocity;
+        }
+
+        // lock right
+        if (rightPressed && runPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX = currentMaxVelocity;
+        }
+        // decelerate to maximum walk velocity 
+        else if (rightPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+            // round to the curentMaxVelocity if within offset
+            if (velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity + 0.05f))
+            {
+                velocityX = currentMaxVelocity;
+            }
+        }
+
+        // round to the currentMaxVelocity if within offset
+        else if (rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 0.05f))
+        {
+            velocityX = currentMaxVelocity;
         }
 
         // set the parameters to our local variable values
